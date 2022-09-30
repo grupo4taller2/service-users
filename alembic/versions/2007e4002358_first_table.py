b7155a81ac7081a707eabd94df1afc1f02907f38
mateoicalvo <macalvo@fi.uba.ar>
2022-09-30 09:35:37 -0300

    Add migration file

--- alembic/versions/2007e4002358_first_table.py
+++ alembic/versions/2007e4002358_first_table.py
@@ -7,6 +7,7 @@ Create Date: 2022-09-30 12:30:43.331459
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -15,10 +16,29 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+F_NAME_MAX_LEN = 64
+L_NAME_MAX_LEN = 64
+EMAIL_MAX_LEN = 64
+USERNAME_MAX_LEN = 30
+HASHED_PASS_LEN = 128
+WALLET_LEN = 128
+
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        'users',
+        sa.Column('id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('username', sa.String(USERNAME_MAX_LEN), nullable=False, unique=True),
+        sa.Column('first_name', sa.String(F_NAME_MAX_LEN), nullable=False),
+        sa.Column('last_name', sa.String(L_NAME_MAX_LEN), nullable=False),
+        sa.Column('email', sa.String(EMAIL_MAX_LEN), nullable=False, unique=True),
+        sa.Column('hashed_password', sa.String(HASHED_PASS_LEN), nullable=False),
+        sa.Column('wallet', sa.String(WALLET_LEN), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table('users')
