3466a5d66f41d12a8e4f77278e3da89159fed514
mateoicalvo <macalvo@fi.uba.ar>
2022-09-30 09:24:13 -0300

    update env

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
+from src.domain.user import User
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
 
