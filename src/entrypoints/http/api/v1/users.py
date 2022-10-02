28c4633eaefa726122804d3748fa8ddc35366543
Mateo <macalvo@fi.uba.ar>
2022-10-02 20:28:16 -0300

    fix created user response

--- src/entrypoints/http/api/v1/users.py
+++ src/entrypoints/http/api/v1/users.py
@@ -49,4 +49,5 @@ async def create_user(req: UserRequest):
     return UserResponse(username=user.username,
                         first_name=user.first_name,
                         last_name=user.last_name,
+                        email=user.email,
                         wallet=user.wallet)
