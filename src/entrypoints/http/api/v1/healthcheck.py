8ed6e360e88986b1f11d859ad34922e0d19bc8d7
mateoicalvo <macalvo@fi.uba.ar>
2022-10-04 01:32:38 -0300

    Correct route

--- src/entrypoints/http/api/v1/healthcheck.py
+++ src/entrypoints/http/api/v1/healthcheck.py
@@ -4,6 +4,6 @@ from fastapi import APIRouter, status
 router = APIRouter()
 
 
-@router.get('/', status_code=status.HTTP_200_OK)
+@router.get('', status_code=status.HTTP_200_OK)
 def health_check():
     return {'message': 'UP'}
