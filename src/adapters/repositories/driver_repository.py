f19ecee0ee157f49bb746866202d39b9c0c3be36
mateoicalvo <macalvo@fi.uba.ar>
2022-10-06 07:50:54 -0300

    Prepare driver repository#

--- src/adapters/repositories/driver_repository.py
+++ src/adapters/repositories/driver_repository.py
@@ -3,33 +3,40 @@ from sqlalchemy.exc import NoResultFound
 
 from src.adapters.repositories.base_repository import BaseRepository
 from src.domain.user import User
-from src.domain.rider import Rider
+from src.domain.driver import Driver
 from src.domain.location import Location
 from src.adapters.repositories.user_dto import UserDTO
-from src.adapters.repositories.rider_dto import RiderDTO
+from src.adapters.repositories.driver_dto import DriverDTO
 
 
-class RiderRepository(BaseRepository):
+class DriverRepository(BaseRepository):
     def __init__(self, session):
         super().__init__()
         self.session: Session = session
 
-    def save(self, rider: Rider):
-        rider_dto = RiderDTO.from_entity(rider)
+    # FIXME: IDEM ABAJO, QUE LLEGUE ACA EL
+    # CAR REPOSITORY CON LA MISMA SESSION PARA GUARDAR
+    # EL AUTO DEL TIPO SI YA EXISTE.
+    # A LO SUMO, SE VA A HACER UPDATE SI ALGO DEL AUTO
+    # CAMBIÓ (PERO NO DEBERÏA PASAR)
+    def save(self, rider: Driver):
+        driver_dto = DriverDTO.from_entity(rider)
         # FIXME: Si no existe el user, crearlo. Rider tiene todos
         # los datos para insertar el user y el rider.
         try:
-            self.session.add(rider_dto)
+            self.session.add(driver_dto)
             self.seen.add(rider)
         except Exception:
             raise
 
-    def find_by_username(self, username: str) -> Rider:
+    # FIXME: CarRepository tendría que llegar acá con
+    # LA MISMA SESSION para que todo sea atómico
+    def find_by_username(self, username: str) -> Driver:
         try:
             user_dto: UserDTO = self.session.query(UserDTO) \
                 .filter_by(username=username).one()
 
-            rider_dto: RiderDTO = self.session.query(RiderDTO) \
+            driver_dto: DriverDTO = self.session.query(DriverDTO) \
                 .filter_by(username=username).one()
         except NoResultFound:
             return None
@@ -41,12 +48,12 @@ class RiderRepository(BaseRepository):
                       email=user.email,
                       password=user.password,
                       events=[],
-                      first_name=rider_dto.first_name,
-                      last_name=rider_dto.last_name,
-                      phone_number=rider_dto.phone_number,
-                      wallet=rider_dto.wallet,
-                      location=Location(rider_dto.preferred_latitude,
-                                        rider_dto.preferred_longitude)
+                      first_name=driver_dto.first_name,
+                      last_name=driver_dto.last_name,
+                      phone_number=driver_dto.phone_number,
+                      wallet=driver_dto.wallet,
+                      location=Location(driver_dto.preferred_latitude,
+                                        driver_dto.preferred_longitude)
                       )
         self.seen.add(rider)
         return rider
