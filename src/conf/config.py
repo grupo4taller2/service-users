b69da1b5e75fa96c1f0cb28b6e454c6079c612a6
mateoicalvo <macalvo@fi.uba.ar>
2022-10-07 18:45:07 -0300

    Move prefix to env var

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,9 +1,10 @@
 from pydantic import BaseSettings
 from typing import List
+import os
 
 
 class Settings(BaseSettings):
     DATABASE_URI: str
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = os.environ.get("API_VERSION_PREFIX")
     CRYPT_CONTEXT_SCHEME: List[str] = ['bcrypt']
     CRYPT_CONTEXT_DEPRECATED: str = 'auto'
