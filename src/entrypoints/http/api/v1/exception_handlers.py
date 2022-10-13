b4c19ede17c97a06edb019643db3fbc501f121f0
mateoicalvo <macalvo@fi.uba.ar>
2022-10-13 07:53:07 -0300

    Implement handler for driver not found

--- src/entrypoints/http/api/v1/exception_handlers.py
+++ src/entrypoints/http/api/v1/exception_handlers.py
@@ -2,12 +2,25 @@ from fastapi import Request
 from fastapi import status
 from fastapi.responses import JSONResponse
 
-from src.serivce_layer.exceptions import UserNotFoundException
+from src.serivce_layer.exceptions import (
+    DriverNotFoundException,
+    UserNotFoundException
+)
 
 
 async def user_not_found_exception(
         request: Request,
         exc: UserNotFoundException) -> JSONResponse:
+
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=str(exc)
+    )
+
+
+async def driver_not_found_exception(
+        request: Request,
+        exc: DriverNotFoundException) -> JSONResponse:
 
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
--- src/entrypoints/http/main.py
+++ src/entrypoints/http/main.py
@@ -4,10 +4,12 @@ from src.conf.config import Settings
 from src.entrypoints.http.api.v1 import api
 
 from src.serivce_layer.exceptions import (
+    DriverNotFoundException,
     UserNotFoundException
 )
 
 from src.entrypoints.http.api.v1.exception_handlers import (
+    driver_not_found_exception,
     user_not_found_exception
 )
 
@@ -19,3 +21,4 @@ app.include_router(api.api_router, prefix=Settings().API_V1_STR)
 app.include_router(root_router)
 
 app.add_exception_handler(UserNotFoundException, user_not_found_exception)
+app.add_exception_handler(DriverNotFoundException, driver_not_found_exception)
