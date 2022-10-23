a1e7d996146775b0742d41ec19674327ef92301d
Mateo <macalvo@fi.uba.ar>
2022-10-22 22:04:13 -0300

    Change responsemodel for admins

--- src/entrypoints/http/api/v1/admins/req_res_admins_models.py
+++ src/entrypoints/http/api/v1/admins/req_res_admins_models.py
@@ -1,10 +1,13 @@
 from pydantic import EmailStr, Field
 from pydantic.main import BaseModel
+from src.entrypoints.http.api.v1.req_res_users_models import (
+    UserResponse
+)
 
 
 class AdminCreateRequest(BaseModel):
     email: EmailStr = Field(example='admin@domain.com')
 
 
-class AdminResponse(BaseModel):
-    email: EmailStr = Field(example='admin@domain.com')
+class AdminResponse(UserResponse):
+    pass
